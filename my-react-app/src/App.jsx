ac38405e71afc354d7a5b67468dfc8d4e8362e97
Ayush Thakur <ayush26819@gmail.com>
2025-06-21 01:45:36 +0530

    new updates

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,44 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import Header from './components/Header'
+import Nav from './components/Nav'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(props) {
+  // const [count, setCount] = useState(0)
 
   return (
+    // <>
+    //   <h1>{props.title}</h1>
+    //   <div>
+    //     <a href="https://vite.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+
+    //   <Test />
+    // </>
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      <Test />
+    <Nav />
+    <Header name="Ayush" color="purple" />
     </>
+    
   )
 }
 
-function Test() {
-  let msg = "A message to test the component Test"
-  return (
-    <p>{msg}</p>
-  )
-}
+// function Test() {
+//   let msg = "A message to test the component Test"
+//   return (
+//     <p>{msg}</p>
+//   )
+// }
 
 export default App
