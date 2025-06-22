5faa881cb57ab0805db860036e1d87dd1a5b5619
Ayush Thakur <ayush26819@gmail.com>
2025-06-22 21:54:36 +0530

    some more notes

--- my-react-app/src/components/Nav.jsx
+++ my-react-app/src/components/Nav.jsx
@@ -1,11 +1,28 @@
+import './Nav.css'
+
 function Nav() {
+    const styleNav = {
+        display: "flex", 
+        justifyContent: "center",
+        alignItem: "center",
+        width: "100vw"
+    };
+
+    const styleUl = {
+        display: "flex", 
+        justifyContent: "center",
+        alignItem: "center",
+        padding: "10px",
+        width: "100vw"
+    };
+
     return (
-        <nav className="main-nav">
-            <ul>
-                <li>Home</li>
-                <li>Articles</li>
-                <li>About</li>
-                <li>Contact</li>
+        <nav style={styleNav} className="main-nav">
+            <ul className='' style={styleUl}>
+                <li className="list">Home</li>
+                <li className="list">Articles</li>
+                <li className="list">About</li>
+                <li className="list">Contact</li>
             </ul>
         </nav>
     );
