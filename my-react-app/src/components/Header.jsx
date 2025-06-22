8217e5f332719c2b9d31e867b68e212019a72afb
Ayush Thakur <ayush26819@gmail.com>
2025-06-22 14:05:23 +0530

    notes on styling jsx

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -1,6 +1,14 @@
+
+
 function Header(props) {
+
+    
+  const style = {
+    color: "yellow",
+  };
+
     console.log(props);
-    return <h1>Hello there mate, {props.name} {props.color}</h1>
+    return <h1 style={style}>Hello there mate, {props.name} {props.color}</h1>
 }
 
 export default Header;
